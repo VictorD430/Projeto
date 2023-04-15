a78b7f96649b90a76a6734c8db555c9b0ff6dcaf
Victor Duque <victor.duque11@hotmail.com>
2023-04-15 09:07:57 -0300

    input e botão de pesquisa criados

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+// import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      btnDisabled: true,
+      loading: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const minChar = 2;
+    this.setState({
+      [name]: value,
+      btnDisabled: value.length < minChar,
+    });
+  };
+
   render() {
+    const { search, btnDisabled, loading } = this.state;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <form>
+          <section className="search-container">
+            <input
+              data-testid="search-artist-input"
+              className="search-artist-input"
+              type="text"
+              placeholder="Nome do Artista"
+              name="search"
+              value={ search }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              onClick={ this.submit }
+              data-testid="search-artist-button"
+              disabled={ btnDisabled }
+            >
+              Pesquisar
+            </button>
+          </section>
+        </form>
+        <Loading loading={ loading } />
+      </div>
     );
   }
 }
