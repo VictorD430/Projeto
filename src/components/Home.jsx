e77f0a6519203bf333ca05fef6b803309447e3a6
Victor Duque <victor.duque11@hotmail.com>
2023-04-15 04:17:21 -0300

    problema de login arrumado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-// import Login from '../pages/Login';
+import Login from '../pages/Login';
 import Search from '../pages/Search';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
@@ -10,22 +10,34 @@ import NotFound from '../pages/NotFound';
 import Header from './Header';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logado: localStorage.getItem('user') !== null,
+    };
+  }
+
   render() {
+    const { logado } = this.state;
     return (
-      <main>
-        <Switch>
-          <Header />
-          <Route exact path="/">
-            <Redirect to="/search" />
-          </Route>
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </main>
+      <>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              { logado ? <Redirect to="/search" />
+                : <Login handleSub={ () => { this.setState({ logado: true }); } } />}
+            </Route>
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
