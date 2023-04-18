2530adc8cab443e906e1ffa52fe5c35ebe570131
Victor Duque <victor.duque11@hotmail.com>
2023-04-17 23:27:12 -0300

    adiciona a pagina de favoritos

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getFavorites();
+  }
+
+  getFavorites = async () => {
+    this.setState({ loading: true });
+    const favorites = await getFavoriteSongs();
+    this.setState({ favorites, loading: false });
+  };
+
+  loadFavorites = () => {
+    this.getFavorites();
+  };
+
   render() {
+    const { favorites, loading } = this.state;
     return (
-      <div data-testid="page-favorites" />
+      loading
+        ? <Loading loading={ loading } />
+        : (
+          <div data-testid="page-favorites">
+            <section className="favorite-list">
+              { favorites.length
+                ? favorites.map(
+                  ({ artist, artwork, previewUrl, trackName, trackId }) => (
+                    <MusicCard
+                      pageFavorites
+                      key={ trackId }
+                      artist={ artist }
+                      artwork={ artwork }
+                      previewUrl={ previewUrl }
+                      trackName={ trackName }
+                      trackId={ trackId }
+                      favoriteList={ this.loadFavorites }
+                    />
+                  ),
+                )
+                : (<span>Nenhum favorito.</span>) }
+            </section>
+          </div>
+        )
     );
   }
 }
